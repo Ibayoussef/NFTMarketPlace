977f83dfdf0a08cbd12dc80cdfd664e33d683947
Ismail Bayoussef <bayoussefismail@gmail.com>
2023-01-28 18:59:47 +0100

    Close #1 setup the NFT contract constructor using ERC721

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTMarketplace {}
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    address owner;
+
+    constructor(address _owner) ERC721("NFTify NFT", "NFY") {
+        owner = _owner;
+    }
+}
